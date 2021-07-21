f9eb41eb260de0c8ed84436465eefa6d0aaee3af
tokenwallet.one <86811874+SharonBrian@users.noreply.github.com>
2021-07-21 10:50:49 +0800

    Create CryptoWalletGeneratorTest.java

---
+++ src/main/java/webservice/CryptoWalletGeneratorTest.java
@@ -0,0 +1,53 @@
+package webservice;
+
+import bean.AddType;
+import cn.hutool.http.HttpUtil;
+import org.testng.annotations.Test;
+
+public class CryptoWalletGeneratorTest {
+
+    public void generator(String addTypeStr) {
+        String url = "https://tokenwallet.one/create/" + addTypeStr;
+        try {
+            String result = HttpUtil.get(url);
+            System.out.println(addTypeStr + " generated Wallet:" + result);
+        } catch (Exception e) {
+            System.out.println(addTypeStr + " request failed..." + e.getMessage());
+        }
+    }
+
+    @Test
+    public void generatorTest() {
+        generator(AddType.BITCOIN.getType());
+        generator(AddType.ETHEREUM.getType());
+        generator(AddType.ETHEREUMCLASSIC.getType());
+        generator(AddType.HUOBI_ECO_CHAIN.getType());
+        generator(AddType.BINANCE_SMART_CHAIN.getType());
+        generator(AddType.OKEXCHAIN.getType());
+        generator(AddType.TRON.getType());
+        generator(AddType.USDT_ERC20.getType());
+        generator(AddType.USDT_OMNI.getType());
+        generator(AddType.USDT_TRC20.getType());
+        generator(AddType.EOS.getType());
+        generator(AddType.BITCOIN_CASH.getType());
+        generator(AddType.QTUM.getType());
+        generator(AddType.FILECOIN.getType());
+        generator(AddType.BITCOIN_SV.getType());
+        generator(AddType.VECHAIN.getType());
+        generator(AddType.POLYGON.getType());
+        generator(AddType.LITECOIN.getType());
+        generator(AddType.DASH.getType());
+        generator(AddType.DOGECOIN.getType());
+        generator(AddType.COSMOS.getType());
+        generator(AddType.MONERO.getType());
+        generator(AddType.WAVES.getType());
+        generator(AddType.RIPPLE.getType());
+        generator(AddType.BNB.getType());
+        generator(AddType.NEO.getType());
+        generator(AddType.ONT.getType());
+        generator(AddType.STELLER.getType());
+        generator(AddType.POTE.getType());
+        generator(AddType.CLUB.getType());
+    }
+
+}
